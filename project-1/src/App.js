cb59489b29477c02ca047d72a38304cf9615035e
Mike <mycelius23@gmail.com>
2020-01-29 14:42:55 -0700

    added language and experience slector inputs.

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <div className="App">
     	<Navigation />
-    	<footer>Group Project 2020.</footer>
+    	<br/>
     </div>
   );
 }
--- project-1/src/Form.js
+++ project-1/src/Form.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import Checkbox from '@material-ui/core/Checkbox';
+import Checkbox from '@material-ui/core/Checkbox';import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 import './App.css';
 
 const Form = (props) => {
@@ -20,14 +19,42 @@ const Form = (props) => {
         	<TextField id="date" label="Date of Birth" type="date"
         defaultValue="2020-01-01" onInput=""  InputLabelProps={{shrink: true,}} margin="normal" variant="outlined"/>
 
+        
+
         	<div className="radio-inputs">
-        		<h4>Gender</h4>
+        	<br/>
+        		
           		<Checkbox onInput="" value="female" name="female"/> Female
       
           		<Checkbox onInput="" value="male" name="male"/> Male
         
-          		<Checkbox onInput="" value="non-binary" name="nonbinary"/> Prefer not to say
+          		<Checkbox onInput="" value="non-binary" name="nonbinary"/> Non-binary
+          		<Checkbox onInput="" value="other" name="other"/> Other
+          		<Checkbox onInput="" value="none" name="none"/> Prefer not to say
             </div>
+            <div>
+      	<br/>
+        <InputLabel id="open-select-label">Choose a Language:</InputLabel>
+        <Select
+          labelId="open-select-label"
+          id="open-select">
+          <MenuItem value={1}>Java</MenuItem>
+          <MenuItem value={2}>Python</MenuItem>
+          <MenuItem value={3}>JavaScript</MenuItem>
+          <MenuItem value={4}>C++</MenuItem>
+        </Select>
+      	<br/>
+      	<br/>
+        <InputLabel id="open-select-label">Level of Experience:</InputLabel>
+        <Select
+          labelId="open-select-label"
+          id="open-select">
+          <MenuItem value={1}>Beginner</MenuItem>
+          <MenuItem value={2}>Mid-Level</MenuItem>
+          <MenuItem value={3}>Advanced</MenuItem>
+        </Select>
+      
+    </div>
           <br/>
         	<div>
         		<Link to="/test" className="submit-btn">{ <Button className="btn" variant="outlined" size="large" onClick={props.handleClick}> submit </Button> }</Link>
